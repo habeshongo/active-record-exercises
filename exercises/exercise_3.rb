517d943d1f9be30838069b3cba11330720c3d70e
habeshongo <doctorarferguson@gmail.com>
2023-07-02 07:29:18 -0300

    [update]-delete the third store

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,12 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+# Load the third store and assign it to @store3
+@store3 = Store.find(3)
+
+# Delete the store from the database
+@store3.destroy
+
+# Output the number of stores using ActiveRecord's count method
+store_count = Store.count
+puts "Number of stores: #{store_count}"
