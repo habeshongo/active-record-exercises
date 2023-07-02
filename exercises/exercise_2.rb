00fc03e4de8b883dd53f2c7235180d25bc57e7aa
habeshongo <doctorarferguson@gmail.com>
2023-07-02 07:27:53 -0300

    [update]-update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# Load the first store with id = 1 and assign it to @store1
+@store1 = Store.find(1)
+
+# Load the second store and assign it to @store2
+@store2 = Store.find(2)
+
+# Update the name of the first store (@store1) in the database
+@store1.update(name: 'New Store Name')
+
+# Output the updated store name
+puts "Updated store name: #{@store1}"
