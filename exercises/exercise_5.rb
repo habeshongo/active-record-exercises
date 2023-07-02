6a4ae3f96cc259ac5965e9d8ff4b889891ad540b
habeshongo <doctorarferguson@gmail.com>
2023-07-02 07:32:59 -0300

    [update]-perform calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+# Total revenue for the entire company
+total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue: $#{total_revenue}"
+
+# Average annual revenue for all stores
+average_revenue = Store.average(:annual_revenue)
+puts "Average Annual Revenue: $#{average_revenue}"
+
+# Number of stores generating $1M or more in annual sales
+high_revenue_stores_count = Store.where("annual_revenue >= ?", 1000000).count
+puts "Number of Stores with Annual Revenue $1M or More: #{high_revenue_stores_count}"
